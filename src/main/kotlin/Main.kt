d70adae467b8f4ea1ba6ce426d52a652ef33ad12
omarmiatello <jackl85@gmail.com>
2019-03-31 16:28:28 +0200

    Add libs: com.github.jacklt:yeelight-kt:1.0.0

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
 package com.github.jacklt.gae.ktor.tg
 
+import com.github.jacklt.yeelight.Yeelight
+import kotlinx.coroutines.runBlocking
+
 fun main() = startApp()
 
+val yeelights = runBlocking { Yeelight.findDevices() }
+    .also { println("Found ${it.size} yeelight!\nApp Started!") }
+
 fun myApp(input: String): String {
+    runBlocking {
+        yeelights.forEach { it.toggle() }
+    }
+
     return "Ciao $input"
 }
