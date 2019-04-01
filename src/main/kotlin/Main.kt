7a50d6d1c805fe35b1e5bd30cdee77ad09fe2244
omarmiatello <jackl85@gmail.com>
2019-04-01 18:24:00 +0200

    calculator-v1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,30 @@ package com.github.jacklt.gae.ktor.tg
 fun main() = startApp()
 
 fun myApp(input: String): String {
-    return "Ciao $input"
+    var res = 0.0
+    var currOp: String? = null
+    input.split(" ").forEach {
+        val num = it.toDoubleOrNull()
+        if (num != null) {
+            if (currOp == null) {
+                res = num
+            } else {
+                res = when (currOp) {
+                    "+" -> res + num
+                    "-" -> res - num
+                    "*" -> res * num
+                    "/" -> res / num
+                    else -> error("Unknown operator $currOp")
+                }
+            }
+        } else {
+            if (it in "+-*/") {
+                currOp = it
+            } else {
+                error("Unknown operator $it")
+            }
+        }
+    }
+
+    return "$input = $res"
 }
